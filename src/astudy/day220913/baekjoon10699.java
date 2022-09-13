70bd9545771dcd2300d8cafdd5b7c6a260e1ae1e
KRDJK <kdf556@naver.com>
2022-09-13 23:58:48 +0900

    [22.09.13] 오랜만에 백준 문제 조금

---
+++ src/astudy/day220913/baekjoon10699.java
@@ -0,0 +1,13 @@
+package astudy.day220913;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class baekjoon10699 {
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+    }
+}
